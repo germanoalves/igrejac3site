46a49c92c096c92b43b9dee4589a3156de9ecc92
Germano Alves <germanomacedoalves@hotmail.com>
2023-08-02 13:34:25 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{astro,html,jsx,tsx,svelte,vue,js,ts}"],
+	darkMode: "class",
+	theme: {
+		extend: {
+			colors: {
+				body: "rgb(var(--color-bg))",
+				"box-bg": "rgb(var(--color-box))",
+				"box-shadow": "rgb(var(--box-sd))",
+				"box-border": "rgb(var(--box-border))",
+				primary: "#1d4ed8",
+				"heading-1": "rgb(var(--heading-1))",
+				"heading-2": "rgb(var(--heading-2))",
+				"heading-3": "rgb(var(--heading-3))",
+			},
+			screens: {
+				sm: "400px",
+				midmd:"880px"
+			},
+		},
+	},
+	plugins: [],
+};
